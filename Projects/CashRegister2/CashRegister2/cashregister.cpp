7d69d5a1bb4cfe05da317157b42fe7f2ff3a43a9
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-03-01 01:57:12 -0800

    Use of Headers

---
+++ Projects/CashRegister2/CashRegister2/cashregister.cpp
@@ -0,0 +1,36 @@
+//
+//  cashregister.cpp
+//  CashRegister2
+//
+//  Created by Erik Argueta on 3/1/20.
+//  Copyright © 2020 Erik Argueta. All rights reserved.
+//
+
+#include "cashregister.h"
+
+CashRegister::CashRegister()
+{
+	clear();
+}
+
+void CashRegister::clear()
+{
+	item_count = 0;
+	total_price = 0;
+}
+
+void CashRegister::add_item(double price)
+{
+	item_count++;
+	total_price = total_price + price;
+}
+
+double CashRegister::get_total() const
+{
+	return total_price;
+}
+
+int CashRegister::get_count() const
+{
+	return item_count;
+}
