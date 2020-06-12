6684553b8991a2d892eeb105ca80f06146b55006
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-06-12 13:05:06 -0700

    Update

---
+++ Labs/Lab006/Lab006/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab006
+//
+//  Created by Erik Argueta on 6/12/20.
+//  Copyright © 2020 Erik Argueta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
